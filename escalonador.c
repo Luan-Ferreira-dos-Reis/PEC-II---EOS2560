2948664e33dc52019c7165b0124a6c1beaccdde7
Luan Ferreira dos Reis <48536463+Luan-Ferreira-dos-Reis@users.noreply.github.com>
2022-10-09 17:09:01 -0300

    Add files via upload

--- escalonador.c
+++ escalonador.c
@@ -6,11 +6,102 @@
 #include <stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
+#include <avr/wdt.h>
 
 /* *INDENT-OFF* */
 #ifdef __cplusplus
     extern "C" {
 #endif
+
+
+
+// Macros para salvar e restaurar o contexto
+#define portRESTORE_CONTEXT()\
+asm volatile (\
+"cli \n\t"\
+"out __SP_L__, %A0 \n\t"\
+"out __SP_H__, %B0 \n\t"\
+"pop r31 \n\t"\
+"pop r30 \n\t"\
+"pop r29 \n\t"\
+"pop r28 \n\t"\
+"pop r27 \n\t"\
+"pop r26 \n\t"\
+"pop r25 \n\t"\
+"pop r24 \n\t"\
+"pop r23 \n\t"\
+"pop r22 \n\t"\
+"pop r21 \n\t"\
+"pop r20 \n\t"\
+"pop r19 \n\t"\
+"pop r18 \n\t"\
+"pop r17 \n\t"\
+"pop r16 \n\t"\
+"pop r15 \n\t"\
+"pop r14 \n\t"\
+"pop r13 \n\t"\
+"pop r12 \n\t"\
+"pop r11 \n\t"\
+"pop r10 \n\t"\
+"pop r9 \n\t"\
+"pop r8 \n\t"\
+"pop r7 \n\t"\
+"pop r6 \n\t"\
+"pop r5 \n\t"\
+"pop r4 \n\t"\
+"pop r3 \n\t"\
+"pop r2 \n\t"\
+"pop r1 \n\t"\
+"pop r0 \n\t"\
+"sei \n\t"\
+"out __SREG__, r0\n\t"\
+"pop r0 \n\t": : "r" (pxCurrentTCB));
+
+#define portSAVE_CONTEXT()\
+asm volatile (\
+"push r0 \n\t"\
+"in r0, __SREG__ \n\t"\
+"cli \n\t"\
+"push r0 \n\t"\
+"push r1 \n\t"\
+"clr r1 \n\t"\
+"push r2 \n\t"\
+"push r3 \n\t"\
+"push r4 \n\t"\
+"push r5 \n\t"\
+"push r6 \n\t"\
+"push r7 \n\t"\
+"push r8 \n\t"\
+"push r9 \n\t"\
+"push r10 \n\t"\
+"push r11 \n\t"\
+"push r12 \n\t"\
+"push r13 \n\t"\
+"push r14 \n\t"\
+"push r15 \n\t"\
+"push r16 \n\t"\
+"push r17 \n\t"\
+"push r18 \n\t"\
+"push r19 \n\t"\
+"push r20 \n\t"\
+"push r21 \n\t"\
+"push r22 \n\t"\
+"push r23 \n\t"\
+"push r24 \n\t"\
+"push r25 \n\t"\
+"push r26 \n\t"\
+"push r27 \n\t"\
+"push r28 \n\t"\
+"push r29 \n\t"\
+"push r30 \n\t"\
+"push r31 \n\t"\
+"in r26, __SP_L__ \n\t"\
+"in r27, __SP_H__ \n\t"\
+"sts pxCurrentTCB+1, r27 \n\t"\
+"sts pxCurrentTCB, r26 \n\t"\
+"sei \n\t" ::);
 
 //constantes do sistema
 #define TEMPO_INTERRUPCAO 0xFFF0
@@ -22,6 +113,10 @@ int tarefa_exec = 0;  // tarefa em execução
 Tarefa **processos;    // buffer de tarefas
 int quantTarefas = 0; //quantidade de tarefas
 int *prazoTarefas;  //deadlines para execução de cada tarefa(em milisegeundos)
+volatile unsigned long pxCurrentTCB;
+
+
+
 
 //configuração do timer do sistema
 void configuraTimer(){
@@ -61,6 +156,7 @@ void setupEOS2560(){
 void executar(){
   while(true){   
     if(processos[tarefa_exec]->execucao == SIM){                                //tarefa deve ser executada
+      //portRESTORE_CONTEXT();//
       processos[tarefa_exec]->codigo();                                         //execucao da tarefa
       prazoTarefas[tarefa_exec] = processos[tarefa_exec]->periodo;              //reinicia prazo para execução
       processos[tarefa_exec]->execucao = NAO;
@@ -91,6 +187,7 @@ void relogio(){
 //função de interrupção ativada por timer que ativa a função relogio do sistema
 ISR(TIMER1_OVF_vect)                              //interrupção do TIMER1 
 {
+  //portSAVE_CONTEXT();//
   relogio();
   TCNT1 = TEMPO_INTERRUPCAO; //reinicia timer
 }
